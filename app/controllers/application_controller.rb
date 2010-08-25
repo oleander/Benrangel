94bcec1ad4c8434e0518eb2e930891e4209ec7c6
Kim Burgestrand <kim@burgestrand.se>
2010-08-25 02:20:31 +0200

    Add #render(Fixnum)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+    def render(code, args = {}, &block)
+      return super unless code.is_a? Fixnum
+      super(:"http/#{code}", args.merge(:status => code), &block)
+    end
 end
