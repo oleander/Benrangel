f710a7ce84cceee43b711446707446ad69170e13
Kim Burgestrand <kim@burgestrand.se>
2010-08-25 18:17:47 +0200

    Add StaticController#view test for 404-pages

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe StaticController do
-  render_views
-  
-  it "renders static pages" do
+  before :each do
     controller.class.view_paths = [ActionView::FixtureResolver.new({
       "layouts/application.html.haml" => "= yield",
-      "static/complex/website.html.haml" => "fake content"
+      "static/complex/website.html.haml" => "fake content",
+      "http/404.html.haml" => '404',
     })]
-    
-    get :view, :path => "complex/website.html"
-    response.body.should match "fake content"
+  end
+  
+  it "renders static pages" do
+    get :view, :path => "complex/website"
+    response.should render_template 'static/complex/website'
+  end
+  
+  it "renders http error pages" do
+    get :view, :path => 'this/should/not/be/found'
+    response.should render_template 'http/404'
   end
 end
