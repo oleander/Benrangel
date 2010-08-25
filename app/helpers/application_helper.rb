946082617df530ef7ccac318cb3f174432184fb8
Kim Burgestrand <kim@burgestrand.se>
2010-08-25 04:11:44 +0200

    Add #yield_default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Renders either the content in the first parameter (given with content_for)
+  # or the partial that is sent as the +default+ parameter.
+  #
+  # @param Symbol symbol The symbol given to #content_for
+  # @param Mixed default Argument given to “render” if there’s no content for +symbol+
+  # @return String
+  def yield_default(symbol, *default)
+    content_for?(symbol) ? content_for(symbol) : render(*default)
+  end
 end
