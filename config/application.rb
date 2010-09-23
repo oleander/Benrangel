805dc340c8850ce98537a2a5e064eca0cf8d4221
Kim Burgestrand <kim@burgestrand.se>
2010-09-23 03:26:28 +0200

    Add jQuery to javascript :defaults

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Benrangel
     config.i18n.default_locale = :sv_SE
     
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(http://code.jquery.com/jquery-1.4.2.min.js)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
