983b7f40c42752d9e73761b515beffc02adbf0e8
Kim Burgestrand <kim@burgestrand.se>
2010-09-23 03:06:50 +0200

    Change to cookie-based session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Benrangel::Application.config.session_store :active_record_store
+Benrangel::Application.config.session_store :cookie_store
